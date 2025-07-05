b4348f882997a8af2ed74705a03021355baf9c31
Christian Helle <christian.helle@outlook.com>
2025-07-05 11:30:13 +0200

    Import models.zig from main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,12 @@
 const models = @import("models.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
     try bw.flush(); // Don't forget to flush!
 }
 
@@ -39,6 +33,4 @@ test "fuzz example" {
 }
 
 const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("openapi2zig_lib");
+const lib = @import("models.zig");
