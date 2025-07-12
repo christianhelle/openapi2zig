3414359fdd42c6c29f4f3ec5d7c1e6a927a7633e
Christian Helle <christian.helle@outlook.com>
2025-07-13 00:45:55 +0200

    Clean up tests

--- src/main.zig
+++ src/main.zig
@@ -12,35 +12,10 @@ pub fn main() !void {
     try bw.flush(); // Don't forget to flush!
 }
 
-const OpenAPI = struct {
-    openapi: []const u8,
-};
-
-test "can deserialize openapi version" {
-    const allocator = std.testing.allocator;
-    const parsed = try std.json.parseFromSlice(OpenAPI, allocator,
-        \\{ "openapi": "3.0.0" }
-    , .{});
-    defer parsed.deinit();
-    try std.testing.expectEqualStrings("3.0.0", parsed.value.openapi);
-}
-
-test "can deserialize petstore.json" {
-    const allocator = std.testing.allocator;
-    const file = try std.fs.cwd().openFile("openapi/petstore.json", .{});
-    defer file.close();
-    try file.seekBy(0);
-    const file_contents = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(file_contents);
-
-    const parsed = try std.json.parseFromSlice(OpenAPI, allocator, file_contents, .{ .ignore_unknown_fields = true });
-    defer parsed.deinit();
-
-    try std.testing.expectEqualStrings("3.0.2", parsed.value.openapi);
-}
-
 test "can deserialize petstore into OpenApiDocument" {
-    const allocator = std.testing.allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     const file = try std.fs.cwd().openFile("openapi/petstore.json", .{});
     defer file.close();
     try file.seekBy(0);
@@ -49,7 +24,15 @@ test "can deserialize petstore into OpenApiDocument" {
 
     var parsed = try models.OpenApiDocument.parse(allocator, file_contents);
     defer parsed.deinit(allocator);
-    
+
     try std.testing.expectEqualStrings("3.0.2", parsed.openapi);
     try std.testing.expectEqualStrings("Swagger Petstore", parsed.info.title);
+
+    std.debug.print("Parsed OpenAPI document: {any}\n", .{parsed});
+
+    var iterator = parsed.paths.path_items.iterator();
+    while (iterator.next()) |path| {
+        std.debug.print("Path: {s}\n", .{path.key_ptr.*});
+        std.debug.print("{any}\n\n", .{path.value_ptr.*});
+    }
 }
