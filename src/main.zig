a4f0c23ba929d4b2a6d7a91d84dcf348a0df0ba8
Christian Helle <christian.helle@outlook.com>
2025-07-14 23:04:36 +0200

    Implement basic CLI argument parsing and generate models

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,50 @@ const generator = @import("generator.zig");
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    //defer _ = gpa.deinit(); // Not needed here, as we will deinit the allocator at the end of the program
+    const allocator = gpa.allocator();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-    try bw.flush(); // Don't forget to flush!
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <path_to_openapi_json> <output_path>\n", .{args[0]});
+        return;
+    }
+
+    const input_file_Path = args[1];
+    const openapi_file = try std.fs.cwd().openFile(input_file_Path, .{});
+    defer openapi_file.close();
+
+    try openapi_file.seekBy(0);
+    const file_contents = try openapi_file.readToEndAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(file_contents);
+
+    var openapi = try models.OpenApiDocument.parse(allocator, file_contents);
+    defer openapi.deinit(allocator);
+
+    var model_generator = generator.ModelCodeGenerator.init(allocator);
+    defer model_generator.deinit();
+
+    const generated_code = try model_generator.generate(openapi);
+    defer allocator.free(generated_code);
+
+    if (args.len > 2) {
+        const output_path = args[2];
+        if (std.fs.path.dirname(output_path)) |dir_path| {
+            try std.fs.cwd().makePath(dir_path);
+        }
+        const output_file = try std.fs.cwd().createFile(output_path, .{ .read = true });
+        defer output_file.close();
+        try output_file.writeAll(generated_code);
+        std.debug.print("Models generated successfully and written to '{s}'.\n", .{output_path});
+    } else {
+        const output_file = try std.fs.cwd().createFile("generated_models.zig", .{ .read = true });
+        defer output_file.close();
+        try output_file.writeAll(generated_code);
+        std.debug.print("Models generated successfully and written to 'generated_models.zig'.\n", .{});
+    }
 }
 
 test "can deserialize petstore into OpenApiDocument" {
