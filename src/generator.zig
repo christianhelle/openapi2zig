f1c5678aec17e5373bdb2e47684820ae8f00ec8f
Christian Helle <christian.helle@outlook.com>
2025-07-16 23:55:50 +0200

    Generate empty POST functions

--- src/generator.zig
+++ src/generator.zig
@@ -80,6 +80,17 @@ pub const ApiCodeGenerator = struct {
                 try parts.append("\n");
                 try parts.append("}\n\n");
             }
+
+            if (path_item.post) |op| {
+                const name = op.operationId orelse try std.fmt.allocPrint(self.allocator, "post_{s}", .{path});
+                const line = try std.fmt.allocPrint(self.allocator, "pub fn {s}(self: *const Self) !void", .{name});
+                try parts.append(line);
+                try parts.append("  {\n");
+                try parts.append("    // Implement POST ");
+                try parts.append(path);
+                try parts.append("\n");
+                try parts.append("}\n\n");
+            }
         }
 
         return try std.mem.join(self.allocator, "", parts.items);
