0f8ef4ea91f182f99a009e43791086e3d664082d
Christian Helle <christian.helle@outlook.com>
2025-07-21 17:58:49 +0200

    Add SwaggerDocument export and v2.0 models namespace to models.zig

--- src/models.zig
+++ src/models.zig
@@ -1,6 +1,8 @@
 // Re-export all model types for easy access
 pub const OpenApiDocument = @import("models/v3.0/openapi.zig").OpenApiDocument;
+pub const SwaggerDocument = @import("models/v2.0/swagger.zig").SwaggerDocument;
 
+// Export v3.0 models
 pub usingnamespace @import("models/v3.0/info.zig");
 pub usingnamespace @import("models/v3.0/server.zig");
 pub usingnamespace @import("models/v3.0/externaldocs.zig");
@@ -17,3 +19,6 @@ pub usingnamespace @import("models/v3.0/link.zig");
 pub usingnamespace @import("models/v3.0/callback.zig");
 pub usingnamespace @import("models/v3.0/security.zig");
 pub usingnamespace @import("models/v3.0/components.zig");
+
+// Export v2.0 models with namespace to avoid conflicts
+pub const v2 = @import("models/v2.0/models.zig");
