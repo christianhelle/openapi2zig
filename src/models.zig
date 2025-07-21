079a58251ecc28ac9dabaad521b4745ddc9f8e73
Christian Helle <christian.helle@outlook.com>
2025-07-21 17:41:27 +0200

    Move models to v3.0 folder

--- src/models.zig
+++ src/models.zig
@@ -1,19 +1,19 @@
 // Re-export all model types for easy access
-pub const OpenApiDocument = @import("models/openapi.zig").OpenApiDocument;
+pub const OpenApiDocument = @import("models/v3.0/openapi.zig").OpenApiDocument;
 
-pub usingnamespace @import("models/info.zig");
-pub usingnamespace @import("models/server.zig");
-pub usingnamespace @import("models/externaldocs.zig");
-pub usingnamespace @import("models/tag.zig");
-pub usingnamespace @import("models/reference.zig");
-pub usingnamespace @import("models/schema.zig");
-pub usingnamespace @import("models/media.zig");
-pub usingnamespace @import("models/parameter.zig");
-pub usingnamespace @import("models/requestbody.zig");
-pub usingnamespace @import("models/response.zig");
-pub usingnamespace @import("models/paths.zig");
-pub usingnamespace @import("models/operation.zig");
-pub usingnamespace @import("models/link.zig");
-pub usingnamespace @import("models/callback.zig");
-pub usingnamespace @import("models/security.zig");
-pub usingnamespace @import("models/components.zig");
+pub usingnamespace @import("models/v3.0/info.zig");
+pub usingnamespace @import("models/v3.0/server.zig");
+pub usingnamespace @import("models/v3.0/externaldocs.zig");
+pub usingnamespace @import("models/v3.0/tag.zig");
+pub usingnamespace @import("models/v3.0/reference.zig");
+pub usingnamespace @import("models/v3.0/schema.zig");
+pub usingnamespace @import("models/v3.0/media.zig");
+pub usingnamespace @import("models/v3.0/parameter.zig");
+pub usingnamespace @import("models/v3.0/requestbody.zig");
+pub usingnamespace @import("models/v3.0/response.zig");
+pub usingnamespace @import("models/v3.0/paths.zig");
+pub usingnamespace @import("models/v3.0/operation.zig");
+pub usingnamespace @import("models/v3.0/link.zig");
+pub usingnamespace @import("models/v3.0/callback.zig");
+pub usingnamespace @import("models/v3.0/security.zig");
+pub usingnamespace @import("models/v3.0/components.zig");
