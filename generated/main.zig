48f0c9d918f509ec990a5e2f41104ee9921b7dbf
Christian Helle <christian.helle@outlook.com>
2025-07-26 22:31:36 +0200

    Update generated main.zig to run getPetById()

--- generated/main.zig
+++ generated/main.zig
@@ -4,4 +4,9 @@ const v3 = @import("generated_v3.zig");
 
 pub fn main() !void {
     std.debug.print("Generated models build and run !!\n", .{});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+    try v3.getPetById(allocator, 0);
 }
