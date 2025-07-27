8e1bc63490cb2d1d1050e89e07772689bf88cd6b
Christian Helle <christian.helle@outlook.com>
2025-07-27 11:57:58 +0200

    format: reformat build.zig for readability

--- build.zig
+++ build.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const version_step = generateVersionStep(b);
+    
     const exe = b.addExecutable(.{
         .name = "openapi2zig",
         .root_source_file = b.path("src/main.zig"),
@@ -11,13 +13,16 @@ pub fn build(b: *std.Build) void {
     });
     exe.step.dependOn(version_step);
     b.installArtifact(exe);
+    
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+    
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+    
     const run_generate_v3_cmd = b.addRunArtifact(exe);
     run_generate_v3_cmd.addArgs(&.{
         "generate",
@@ -30,6 +35,7 @@ pub fn build(b: *std.Build) void {
     });
     const run_generate_v3_step = b.step("run-generate-v3", "Run the app with generate command");
     run_generate_v3_step.dependOn(&run_generate_v3_cmd.step);
+    
     const run_generate_v2_cmd = b.addRunArtifact(exe);
     run_generate_v2_cmd.addArgs(&.{
         "generate",
@@ -42,20 +48,24 @@ pub fn build(b: *std.Build) void {
     });
     const run_generate_v2_step = b.step("run-generate-v2", "Run the app with generate command");
     run_generate_v2_step.dependOn(&run_generate_v2_cmd.step);
+    
     const run_generate = b.step("run-generate", "Run the app with generate commands");
     run_generate.dependOn(&run_generate_v3_cmd.step);
     run_generate.dependOn(&run_generate_v2_cmd.step);
+    
     const tests_mod = b.createModule(.{
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
+    
     const exe_unit_tests = b.addTest(.{
         .root_module = tests_mod,
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+    
     const test_artifact = b.addInstallArtifact(
         exe_unit_tests,
         .{ .dest_dir = .{ .override = .{ .custom = "tests" } } },
