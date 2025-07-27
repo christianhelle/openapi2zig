341cef69faf040b1c6095211cc9ac6157818e32a
Christian Helle <christian.helle@outlook.com>
2025-07-27 11:59:04 +0200

    format: reformat v3.0/externaldocs.zig for readability

--- src/models/v3.0/externaldocs.zig
+++ src/models/v3.0/externaldocs.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const json = std.json;
+
 pub const ExternalDocumentation = struct {
     url: []const u8,
     description: ?[]const u8 = null,
+
     pub fn parseFromJson(allocator: std.mem.Allocator, value: json.Value) anyerror!ExternalDocumentation {
         const obj = value.object;
         return ExternalDocumentation{
@@ -10,6 +12,7 @@ pub const ExternalDocumentation = struct {
             .description = if (obj.get("description")) |val| try allocator.dupe(u8, val.string) else null,
         };
     }
+
     pub fn deinit(self: ExternalDocumentation, allocator: std.mem.Allocator) void {
         allocator.free(self.url);
         if (self.description) |desc| allocator.free(desc);
