7c042dec73bea8c53dd2a174405bfb43a54f9caa
Christian Helle <christian.helle@outlook.com>
2025-07-27 11:55:47 +0200

    format: reformat v2.0/externaldocs.zig for readability

--- src/models/v2.0/externaldocs.zig
+++ src/models/v2.0/externaldocs.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const json = std.json;
+
 pub const ExternalDocumentation = struct {
     url: []const u8,
     description: ?[]const u8 = null,
+
+    pub fn parseFromJson(allocator: std.mem.Allocator, value: json.Value) anyerror!ExternalDocumentation {
+        return ExternalDocumentation{
+            .url = try allocator.dupe(u8, value.object.get("url").?.string),
+            .description = if (value.object.get("description")) |val| try allocator.dupe(u8, val.string) else null,
+        };
+    }
+
     pub fn deinit(self: *ExternalDocumentation, allocator: std.mem.Allocator) void {
         allocator.free(self.url);
-        if (self.description) |description| {
-            allocator.free(description);
-        }
-    }
-    pub fn parseFromJson(allocator: std.mem.Allocator, value: json.Value) anyerror!ExternalDocumentation {
-        const url = try allocator.dupe(u8, value.object.get("url").?.string);
-        const description = if (value.object.get("description")) |val| try allocator.dupe(u8, val.string) else null;
-        return ExternalDocumentation{
-            .url = url,
-            .description = description,
-        };
+        if (self.description) |description| allocator.free(description);
     }
 };
