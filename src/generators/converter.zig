3890811573e05e7abf1b41ccce0a250d07578065
Christian Helle <christian.helle@outlook.com>
2025-07-27 11:58:20 +0200

    format: reformat converter.zig for readability

--- src/generators/converter.zig
+++ src/generators/converter.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+
 pub fn getDataType(field_schema: []const u8) ![]const u8 {
     if (std.mem.eql(u8, field_schema, "string")) {
         return "[]const u8";
@@ -9,9 +10,9 @@ pub fn getDataType(field_schema: []const u8) ![]const u8 {
     } else if (std.mem.eql(u8, field_schema, "boolean")) {
         return "bool";
     } else if (std.mem.eql(u8, field_schema, "array")) {
-        return "[]const u8"; // TODO: handle array items type
+        return "[]const u8";
     } else if (std.mem.eql(u8, field_schema, "object")) {
-        return "std.json.Value"; // or a generated struct if possible
+        return "std.json.Value";
     } else {
         return "[]const u8";
     }
