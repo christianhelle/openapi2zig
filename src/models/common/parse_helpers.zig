365118656475694964047730b3d68bed25926d8b
Christian Helle <christian.helle@outlook.com>
2026-07-05 00:04:17 +0200

    Clean up partially parsed items in parseArray on error

--- src/models/common/parse_helpers.zig
+++ src/models/common/parse_helpers.zig
@@ -3,7 +3,14 @@ const json = std.json;
 
 pub fn parseArray(comptime T: type, allocator: std.mem.Allocator, value: json.Value) anyerror![]T {
     var array_list = std.ArrayList(T).empty;
-    errdefer array_list.deinit(allocator);
+    errdefer {
+        for (array_list.items) |*item| {
+            if (@hasDecl(T, "deinit")) {
+                item.deinit(allocator);
+            }
+        }
+        array_list.deinit(allocator);
+    }
     for (value.array.items) |item| {
         try array_list.append(allocator, try T.parseFromJson(allocator, item));
     }
