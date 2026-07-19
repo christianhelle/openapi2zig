07cc83c84eb6798e6d0c6e0aec772a202faa32e0
Christian Helle <christian.helle@outlook.com>
2026-07-19 23:51:51 +0200

    Update run-generate build task to generate anthropic.zig

--- build.zig
+++ build.zig
@@ -160,6 +160,20 @@ pub fn build(b: *std.Build) void {
     const run_generate_lmstudio_json_step = b.step("run-generate-lmstudio", "Run the app with generate command for LM Studio OpenAPI JSON");
     run_generate_lmstudio_json_step.dependOn(&run_generate_v3_yaml_cmd.step);
 
+    const run_generate_anthropic_json_cmd = b.addRunArtifact(exe);
+    run_generate_anthropic_json_cmd.addArgs(&.{
+        "generate",
+        "-i",
+        "openapi/v3.1/anthropic.json",
+        "-o",
+        "generated/anthropic.zig",
+        "--base-url",
+        "http://localhost:1234",
+    });
+
+    const run_generate_anthropic_json_step = b.step("run-generate-anthropic", "Run the app with generate command for Anthropic OpenAPI JSON");
+    run_generate_anthropic_json_step.dependOn(&run_generate_lmstudio_json_cmd.step);
+
     const run_generate = b.step("run-generate", "Run the app with generate commands");
     run_generate.dependOn(&run_generate_v3_cmd.step);
     run_generate.dependOn(&run_generate_v3_yaml_cmd.step);
@@ -169,6 +183,7 @@ pub fn build(b: *std.Build) void {
     run_generate.dependOn(&run_generate_v31_cmd.step);
     run_generate.dependOn(&run_generate_v31_yaml_cmd.step);
     run_generate.dependOn(&run_generate_lmstudio_json_cmd.step);
+    run_generate.dependOn(&run_generate_anthropic_json_cmd.step);
 
     const tests_mod = b.createModule(.{
         .root_source_file = b.path("src/tests.zig"),
