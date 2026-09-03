695d7de9b4aa9ba2817ae04391d406081466e1e5
Christian Helle <christian.helle@outlook.com>
2026-09-03 13:22:40 +0200

    fix the custom-directory example printed by Show-Usage too

--- docs/install.ps1
+++ docs/install.ps1
@@ -100,7 +100,7 @@ function Show-Usage
   Write-Host "  irm https://christianhelle.com/openapi2zig/install.ps1 | iex" -ForegroundColor "White"
   Write-Host ""
   Write-Host "  # Custom directory" -ForegroundColor "Gray"
-  Write-Host "  irm https://christianhelle.com/openapi2zig/install.ps1 | iex -InstallDir 'C:\Tools'" -ForegroundColor "White"
+  Write-Host "  & ([scriptblock]::Create((irm https://christianhelle.com/openapi2zig/install.ps1))) -InstallDir 'C:\Tools'" -ForegroundColor "White"
   Write-Host ""
 }
 
